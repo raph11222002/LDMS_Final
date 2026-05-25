3ac2d3bc37d20f85c19dfc1e902f23d97be70ad4
jhona <jhonabylle@gmail.com>
2026-05-25 23:03:46 +0800

    configure render port

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services to the container.
 builder.Services.AddScoped<OrderService>();
 
